c5044b13f635f249b26f70a56572ed58efd909a1
szdani7 <szdani777@gmail.com>
2018-05-06 21:51:18 +0200

    Removing duplicated folder

--- outlay/app/src/main/java/app/outlay/view/progress/ProgressLayoutListener.java
+++
@@ -1,9 +0,0 @@
-package app.outlay.view.progress;
-
-/**
- * Created by Bogdan Melnychuk on 1/31/16.
- */
-public interface ProgressLayoutListener {
-    void onProgressCompleted();
-    void onProgressChanged(int seconds);
-}
